ba09ac05a1a752142da9ea161967f25f64cec10f
joao.goncalves <joao.goncalves@leponodobrasil.con.br>
2026-07-07 10:02:43 -0300

    ajuste no cabecalho para nao precisar fazer transformacoes no powerquery

--- main.py
+++ main.py
@@ -10,4 +10,17 @@ df = yf.download(
     start='2017-01-01',
     end=datetime.today().strftime('%Y-%m-%d')
 )
-df.to_csv('dolar.csv')
+
+# 1. Remove o segundo nível do cabeçalho (o "BRL=X")
+df.columns = df.columns.droplevel(1)
+df.columns.name = None
+
+# === A MÁGICA PARA FICAR NA MESMA LINHA ===
+# Transforma o índice 'Date' em uma coluna normal
+df_alinhado = df.reset_index()
+# ==========================================
+
+print(df_alinhado.head())
+
+# Salva o arquivo CSV perfeitamente alinhado na primeira linha
+df_alinhado.to_csv('dolar.csv', index=False) # index=False evita criar uma coluna extra sem nome
